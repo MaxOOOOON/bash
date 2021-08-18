c5b2a9001e9a1c769318738a284e3cd8c769bebe
MaxOOOOON <lisok.maksim@gmail.com>
2021-08-19 01:31:57 +0300

    some changes

--- files/mail.sh
+++ files/mail.sh
@@ -1,7 +1,30 @@
 #!/bin/bash
-lastaccess=$(tail -n 1 /vagrant_files/access-4560-644067.log | awk '{print $4}')
-#grep -n 'http://www.yahoo.com' file1
+last_run=`cat /var/tmp/last_run`
+
+if ls /var/tmp/last_run 1> /dev/null && grep -n $last_run /vagrant_files/access-4560-644067.log 1> /dev/null; then 
+first_line=$(grep -n $last_run /vagrant_files/access-4560-644067.log | head -n 1 | cut -d: -f -1)
+from_time=$last_run   
+else 
+first_line=0
+from_time=$(cut -f 4 -d ' ' /vagrant_files/access-4560-644067.log | head -n 1)
+from_time=${from_time:1}
+fi
 
 
-/vagrant_files/script.sh /vagrant_files/access-4560-644067.log > /var/tmp/result
-sendmail -v vanya.9werty@gmail.com < /var/tmp/result
+#time range
+to_time=$(cut -f 4 -d ' ' /vagrant_files/access-4560-644067.log | tail -n 1)
+
+
+
+
+echo "Time range from $from_time to ${to_time:1}" > /var/tmp/result
+echo "----------------------------------------------" >> /var/tmp/result
+/vagrant_files/script.sh /vagrant_files/access-4560-644067.log $first_line >> /var/tmp/result
+#sendmail -v vanya.9werty@gmail.com < /var/tmp/result
+
+
+
+#useful commands
+#  grep -n "182.254.243.249" /vagrant_files/access-4560-644067.log | grep -E '15/Aug/2019:00:24:38'\
+#  echo ${v%:*} # убрать всё после последнего двоеточия
+#  echo ${v#*:} # убрать всё до первого двоеточия
--- files/script.sh
+++ files/script.sh
@@ -1,14 +1,19 @@
 #!/bin/bash
-logfile=$1
+
+logfile=`tail -n +$2 $1`
 
 echo "IP addesses with the number of  request:"
-cat $logfile | cut -d ' ' -f 1 | sort | uniq -c | sort -nr | head -n 10
+echo "$logfile" | cut -d ' ' -f 1 | sort | uniq -c | sort -nr | head -n 10
 
 echo "Addresses with the number of  request:"
-cat $logfile | awk '{print $7}' | sort | uniq -c | sort -nr | head -n 10
+echo "$logfile" | awk '{print $7}' | sort | uniq -c | sort -nr | head -n 10
 
 echo "Errors since the last launch:"
-cat $logfile | awk '{print $9}' | sort | grep -P '^4|^5' | uniq 
+echo "$logfile" | awk '{print $9}' | sort | grep -P '^4|^5' | uniq 
 
 echo "List all codes:"
-cat $logfile | awk '{print $9}' | sort | grep -P '[0-9]' | uniq -c
+echo "$logfile" | awk '{print $9}' | sort | grep -P '[0-9]' | uniq -c
+
+
+last_run=$(tail -n 1 $1 | awk '{print $4}')
+echo ${last_run:1} > /var/tmp/last_run
