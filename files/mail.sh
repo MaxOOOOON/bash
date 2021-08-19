3e776ce929491e6025c1f08142d5eda42fec6df4
MaxOOOOON <lisok.maksim@gmail.com>
2021-08-19 23:30:43 +0300

    readme and write lock script

--- files/mail.sh
+++ files/mail.sh
@@ -1,30 +1,54 @@
-#!/bin/bash
-last_run=`cat /var/tmp/last_run`
+#!/usr/bin/env bash 
 
-if ls /var/tmp/last_run 1> /dev/null && grep -n $last_run /vagrant_files/access-4560-644067.log 1> /dev/null; then 
-first_line=$(grep -n $last_run /vagrant_files/access-4560-644067.log | head -n 1 | cut -d: -f -1)
+LOCKFILE=/tmp/lock.txt
+if [ -e ${LOCKFILE} ] && kill -0 `cat ${LOCKFILE}`; then
+    echo "already running"
+    exit
+fi
+
+
+trap "rm -f ${LOCKFILE}; exit" INT TERM EXIT
+echo $$ > ${LOCKFILE}
+
+#Подавление вывода команд
+exec &> /dev/null
+
+last_run=`cat /var/tmp/last_run`
+logfile=/vagrant_files/access-4560-644067.log
+
+#Проверка на предыдущий запуск и есть ли последняя запись, с предыдущего запуска, в логе
+#Если скрипт уже выполнялся, то получаем первую строку для обработки и время первой строки лога  
+#Иначе лог читается с первой строки 
+
+if ls /var/tmp/last_run  && grep -n $last_run $logfile; then 
+first_line=$(grep -n $last_run $logfile | head -n 1 | cut -d: -f -1)
 from_time=$last_run   
 else 
 first_line=0
-from_time=$(cut -f 4 -d ' ' /vagrant_files/access-4560-644067.log | head -n 1)
+from_time=$(cut -f 4 -d ' ' $logfile | head -n 1)
 from_time=${from_time:1}
 fi
 
 
-#time range
-to_time=$(cut -f 4 -d ' ' /vagrant_files/access-4560-644067.log | tail -n 1)
+#Время последней строки лога
+to_time=$(cut -f 4 -d ' ' $logfile | tail -n 1)
 
 
 
-
+#Запись временного диапазона
 echo "Time range from $from_time to ${to_time:1}" > /var/tmp/result
 echo "----------------------------------------------" >> /var/tmp/result
-/vagrant_files/script.sh /vagrant_files/access-4560-644067.log $first_line >> /var/tmp/result
-#sendmail -v vanya.9werty@gmail.com < /var/tmp/result
+#Выполнение скрипта с передачей параметров лог файла и первой строки дял чтения
+/vagrant_files/script.sh $logfile $first_line >> /var/tmp/result
+#Отправляем данные из файла на почту
+sendmail -v ivanov@gmail.com < /var/tmp/result
 
-
+rm -f ${LOCKFILE}
 
 #useful commands
 #  grep -n "182.254.243.249" /vagrant_files/access-4560-644067.log | grep -E '15/Aug/2019:00:24:38'\
+#  awk '/yahoo/{y=1;next}y' data.txt
 #  echo ${v%:*} # убрать всё после последнего двоеточия
 #  echo ${v#*:} # убрать всё до первого двоеточия
+#exec 3>myfile
+#echo "and this should be stored in the file" >&3
--- files/script.sh
+++ files/script.sh
@@ -1,19 +1,24 @@
-#!/bin/bash
+#!/usr/bin/env bash 
 
+#Получение необходимого диапазона лога
 logfile=`tail -n +$2 $1`
 
+#X IP адресов (с наибольшим кол-вом запросов) с указанием кол-ва запросов c момента последнего запуска скрипта;
 echo "IP addesses with the number of  request:"
 echo "$logfile" | cut -d ' ' -f 1 | sort | uniq -c | sort -nr | head -n 10
 
+#Y запрашиваемых адресов (с наибольшим кол-вом запросов) с указанием кол-ва запросов c момента последнего запуска скрипта;
 echo "Addresses with the number of  request:"
 echo "$logfile" | awk '{print $7}' | sort | uniq -c | sort -nr | head -n 10
 
+# все ошибки c момента последнего запуска;
 echo "Errors since the last launch:"
 echo "$logfile" | awk '{print $9}' | sort | grep -P '^4|^5' | uniq 
 
+#список всех кодов возврата с указанием их кол-ва с момента последнего запуска.
 echo "List all codes:"
 echo "$logfile" | awk '{print $9}' | sort | grep -P '[0-9]' | uniq -c
 
-
+#Запись времени последней записи лога
 last_run=$(tail -n 1 $1 | awk '{print $4}')
 echo ${last_run:1} > /var/tmp/last_run
